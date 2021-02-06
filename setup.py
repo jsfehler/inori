78a5ab2af68441739cec6282b4263c97a5bad76a
Joshua Fehler <jsfehler@gmail.com>
2021-02-05 21:52:19 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(filename:str):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, 'r') as f:
+        return f.read()
+
+
+setup(
+    name="birdman",
+    version="0.0.1",
+    description="The Universal API Client Constructor.",
+    long_description=read('README.rst'),
+    author="Joshua Fehler",
+    license="GPLv3",
+    url="https://github.com/jsfehler/birdman",
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.25.1',
+        'shibari==0.0.2',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+)
