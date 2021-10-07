6b68f5dd669b2227982949630667990841964a74
Joshua Fehler <jsfehler@gmail.com>
2021-10-07 17:34:29 -0400

    Bump version to 0.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Joshua Fehler'
 author = 'Joshua Fehler'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.2'
+release = 'v0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename:str):
 
 setup(
     name="inori",
-    version="0.0.2",
+    version="0.0.3",
     description="The Universal API Client Constructor.",
     long_description=read('README.rst'),
     author="Joshua Fehler",
