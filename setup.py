14600a3168b3ff82a5896a7139e69827e3a8797f
Joshua Fehler <jsfehler@gmail.com>
2023-02-28 12:43:46 -0500

    Bump version to 0.0.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(filename: str):
 
 setup(
     name="inori",
-    version="0.0.9",
+    version="0.0.10",
     description="The Universal API Client Constructor.",
     long_description=read('README.rst'),
     author="Joshua Fehler",
