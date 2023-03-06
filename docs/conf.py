558e50868af43aca9e5a58bd0500e69d1c343a6b
Joshua Fehler <jsfehler@gmail.com>
2023-03-06 10:37:33 -0500

    Bump version to 0.0.11

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Joshua Fehler'
 author = 'Joshua Fehler'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.10'
+release = 'v0.0.11'
 
 
 # -- General configuration ---------------------------------------------------
